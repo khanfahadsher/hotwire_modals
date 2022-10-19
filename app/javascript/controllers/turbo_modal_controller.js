1ec0ae28631c458561c77ea8830a1f97a1ee4b17
Fahad Khan <khanfahadsher@gmail.com>
2022-10-19 18:37:17 +0500

    Handle form validation

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -6,58 +6,110 @@ export default class extends Controller {
   static targets = ['personal', 'form']
 
   connect () {
-    this.element.addEventListener("turbo:submit-start", e => this.validate(e))
+    this.element.addEventListener("turbo:submit-start", e => this.personal_validate(e))
+    this.element.addEventListener("turbo:submit-start", e => this.employment_validate(e))
   }
 
-  validate (event) {
-    $(".personal_contents").validate({
-      rules: {
-          first_name: "required",
-          last_name: "required",
-          email_address: "required",
-          phone_number: "required"
-      },
-      messages: {
-          first_name: "Please enter first name",
-          last_name: "Please enter last name",
-          email_address: "Please enter email address",
-          phone_number: "Please enter phone number",
-      }
-    })
+  personal_validate (event) {
     var first_name = document.getElementById('first-name').value;
     var last_name = document.getElementById('last-name').value;
     var email_address = document.getElementById('email-address').value;
     var phone_number = document.getElementById('phone-number').value;
-    if (email_address != null) {
-      var valid_email = email.toLowerCase()
+    if (first_name == ""){
+      document.getElementById('first_name_error').textContent = "First name field is required";
+      document.getElementById('first_name_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('first_name_error').textContent = ""
+    }
+    if (last_name == ""){
+      document.getElementById('last_name_error').textContent = "Last name field is required";
+      document.getElementById('last_name_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('last_name_error').textContent = ""
+    }
+
+    if (email_address != "") {
+      var email_check = "true"
+      var valid_email = email_address.toLowerCase()
       .match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
-      if (valid_email != null)
+      if (valid_email == null)
       {
-        email_address = "true"
-      }
-      else {
-        email_address = null
-        document.getElementById('email_error').text("Email format is not correct");
-        document.getElementById('email_error').css('color','red');
+        email_check == 'false'
+        document.getElementById('email_error').textContent = ""
+        document.getElementById('email_error').textContent = "Email format is not correct";
+        document.getElementById('email_error').style.color = 'red'
         event.detail.formSubmission.stop()
         return
       }
-    } 
-    else{
-      document.getElementById('email_error').text("Email field is required");
-      document.getElementById('email_error').css('color','red');
-      event.detail.formSubmission.stop()
-      return
-    }
-    if (first_name == null){
+      else{
+        document.getElementById('email_error').textContent = ""
+      }
 
-    }
-    if (true) {
+      if (email_check == 'true'){
+        document.getElementById('email_error').textContent = ""
+      }
+    } 
+    else if (email_address == ""){
+      document.getElementById('email_error').textContent = "Email field is required";
+      document.getElementById('email_error').style.color = 'red'
       event.detail.formSubmission.stop()
       return
     }
+    else{
+      document.getElementById('email_error').textContent = ""
+    }
+    if (phone_number == ""){
+      document.getElementById('phone_error').textContent = "Phone field is required";
+      document.getElementById('phone_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('phone_error').textContent = ""
+    }
+  }
+
+  employment_validate(event) {
+    var employment = document.getElementById('employer').value;
+    var date_started = document.getElementById('date-started').value;
+    var date_ended = document.getElementById('date-employment-ended').value;
+    if (employment == ""){
+      document.getElementById('employment_error').textContent = "Employment name field is required";
+      document.getElementById('employment_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('employment_error').textContent = ""
+    }
+    if (date_started == ""){
+      document.getElementById('date_started_error').textContent = "Date field is required";
+      document.getElementById('date_started_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('date_started_error').textContent = ""
+    }
+
+    if (date_ended == ""){
+      document.getElementById('date_ended_error').textContent = "Date field is required";
+      document.getElementById('date_ended_error').style.color = 'red'
+      event.detail.formSubmission.stop()
+      return
+    }
+    else{
+      document.getElementById('date_ended_error').textContent = ""
+    }
+
   }
 
   hideModal() {
@@ -71,13 +123,4 @@ export default class extends Controller {
     }
   }
   
-  personal_validations(e) {
-    debugger
-    
-
-    
-    if (e.detail.success) {
-      this.hideModal()
-    }
-  }
 }
