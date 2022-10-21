922da28b6888a8f0075286d7889b0ad478a3495d
Fahad Khan <khanfahadsher@gmail.com>
2022-10-21 16:29:43 +0500

    Feedbacks

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
 
@@ -60,7 +59,7 @@ export default class extends Controller {
       );
       if (valid_email == null)
       {
-        email_check == 'false'
+        email_check = 'false'
         document.getElementById('email_error').textContent = ""
         document.getElementById('email_error').textContent = "Email format is not correct";
         document.getElementById('email_error').style.color = 'red'
@@ -84,14 +83,62 @@ export default class extends Controller {
     else{
       document.getElementById('email_error').textContent = ""
     }
-    if (phone_number == ""){
-      document.getElementById('phone_error').textContent = "Phone field is required";
-      document.getElementById('phone_error').style.color = 'red'
-      event.detail.formSubmission.stop()
-      return
+    if (phone_number != "") {
+      var phone_check = "true"
+      var valid_phone = phone_number.match('[0-9]{3}-[0-9]{3}-[0-9]{4}');
+      if (valid_phone == null)
+      {
+        phone_check == 'false'
+        document.getElementById('phone_error').textContent = ""
+        document.getElementById('phone_error').textContent = "Phone format is not correct";
+        document.getElementById('phone_error').style.color = 'red'
+        event.detail.formSubmission.stop()
+        return
+      }
+      else{
+        document.getElementById('phone_error').textContent = ""
+      }
+
+      if (phone_check == 'true'){
+        document.getElementById('phone_error').textContent = ""
+      }
+    } 
+  }
+
+  email_validate(){
+    var email_address = document.getElementById('email-address').value;
+    if (email_address != "") {
+      var email_check = "true"
+      var valid_email = email_address.toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+      if (valid_email == null)
+      {
+        email_check = 'false'
+        document.getElementById('email_error').textContent = ""
+        document.getElementById('email_error').textContent = "Email format is not correct";
+        document.getElementById('email_error').style.color = 'red'
+        $('#phone-number').attr('disabled','disabled');
+      }
+      else{
+        document.getElementById('email_error').textContent = ""
+        $('#phone-number').removeAttr('disabled');
+      }
+
+      if (email_check == 'true'){
+        document.getElementById('email_error').textContent = ""
+        $('#phone-number').removeAttr('disabled');
+      }
+    } 
+    else if (email_address == ""){
+      document.getElementById('email_error').textContent = "Email field is required";
+      document.getElementById('email_error').style.color = 'red'
+      $('#phone-number').attr('disabled','disabled');
     }
     else{
-      document.getElementById('phone_error').textContent = ""
+      document.getElementById('email_error').textContent = ""
+      $('#phone-number').removeAttr('disabled');
     }
   }
 
